22ec6b187e3a92fa0c95898ae5e43cefc94a9631
Juan Carlos <jc29052002@gmail.com>
2023-12-18 22:27:44 -0300

    Add files via upload

---
+++ Novo livro/js/main.js
@@ -0,0 +1,54 @@
+const fundo = document.getElementById("fundo")
+const conteudo = document.getElementById("conteudo")
+const menu = document.getElementById("menu")
+let ultimoFundo  = "fundo-dia"
+let isConteudoEscuro = false
+
+function removeFundoAnterior(){
+    fundo.classList.remove(ultimoFundo)
+}
+
+function mudarFundoDia() {
+    removeFundoAnterior()
+    fundo.classList.add("fundo-dia")
+    menu.style.color = 'black'
+    if (isConteudoEscuro){
+        isConteudoEscuro = false
+        conteudo.classList.remove("escuro-texto")
+    }
+    ultimoFundo = "fundo-dia"
+}
+
+function mudarFundoNoite() {
+    removeFundoAnterior()
+    menu.style.color = 'white'
+    fundo.classList.add("fundo-noite")
+
+    if (isConteudoEscuro === false ){
+        isConteudoEscuro = true
+        conteudo.classList.add("escuro-texto")
+    }
+    ultimoFundo = "fundo-noite"
+}
+
+function ativaModoClaro() {
+    removeFundoAnterior()
+    fundo.classList.add("modo-claro")
+    menu.style.color = 'black'
+    if (isConteudoEscuro){
+        isConteudoEscuro = false
+        conteudo.classList.remove("escuro-texto")
+    }
+    ultimoFundo = "modo-claro"
+}
+
+function ativarModoEscuro() {
+    removeFundoAnterior()
+    menu.style.color = 'white'
+    fundo.classList.add("modo-escuro")
+    if (isConteudoEscuro === false ){
+        isConteudoEscuro = true
+        conteudo.classList.add("escuro-texto")
+    }
+    ultimoFundo = "modo-escuro"
+}
